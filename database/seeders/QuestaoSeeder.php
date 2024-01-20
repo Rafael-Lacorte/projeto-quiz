b2016b9c61939dfb02aa939868712cfa88c01020
Rafael-Lacorte <rafaellacorte14@gmail.com>
2024-01-20 16:46:19 -0300

    commit

--- database/seeders/QuestaoSeeder.php
+++ database/seeders/QuestaoSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class QuestaoSeeder extends Seeder
 {
@@ -12,6 +12,55 @@ class QuestaoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $questoes = [
+            [
+                'texto_pergunta' => 'Qual a capital da Nigeria',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Alemanha',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital do Colombia',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital dos Estados Unidos da America',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Turquia',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Inglaterra',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Holanda',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Belgica',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital do Japão',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Arábia Saudita',
+                'materia_id' =>    1
+            ],
+            [
+                'texto_pergunta' => 'Qual a capital da Coreia do Sul',
+                'materia_id' =>    1
+            ],
+        ];
+        foreach ($questoes as $questao) {
+            DB::table('questao')->insert($questao);
+        }
     }
+
 }
